a4de23bc8aedf86b6d2b5fbcb477db5c313f9644
claudia njeri <claudianjeri04@gmail.com>
2018-04-22 21:40:05 +0300

    deployment

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -26,4 +26,15 @@ export class QuotesComponent implements OnInit{
    this.quotes.push(quote)
  }
 
+ 
+ deleteGoal(isComplete,index){
+  if (isComplete){
+      let toDelete=confirm(`Are you sure you want to delete ${this.quotes[index].quote}`)
+      
+      if(toDelete){
+          this.quotes.splice(index,1)
+      }
+  }
+}
+
 }
