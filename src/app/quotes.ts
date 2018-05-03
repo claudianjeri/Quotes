8e142d556919948963a56845dc5c915b85ac2f7a
claudia njeri <claudianjeri04@gmail.com>
2018-05-03 11:34:54 +0300

    add styling

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,12 @@
 export class Quotes {
-    public showAuthor:boolean;
-    constructor(public id: number, public quote: string, public author: string, public date: Date )
+    public showAuthor:boolean; 
+    constructor(public id: number, public quote: string, public author: string, public date: Date, public upvotes:number, public downvotes:number )
     
     { this.showAuthor=false}
+    upVote(){
+        this.upvotes +=1
+    }
+    downVote(){
+        this.downvotes +=1
+    }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -12,12 +12,12 @@ export class QuotesComponent implements OnInit{
   }
 
   quotes = [
-    new Quotes( 1,'We must balance conspicuous consumption with conscious capitalism.',  "Kevin Kruse", new Date(2018, 5, 4)),
-    new Quotes(2, "Life is what happens to you while you’re busy making other plans.", "John Lennon", new Date(2018, 6, 8)),
-    new Quotes( 3,"We become what we think about.", "Earl Nightingale", new Date(2018, 6, 9)),
-    new Quotes(5, "Life is 10% what happens to me and 90% of how I react to it.", "Charles Swindoll", new Date(2018, 1, 12)),
-    new Quotes(6, "The most common way people give up their power is by thinking they don’t have any.", "Alice Walker", new Date(2018, 12, 5)),
-    new Quotes(7, "The mind is everything. What you think you become.", "Buddha", new Date(2018, 4, 4))
+    new Quotes( 1,'We must balance conspicuous consumption with conscious capitalism.',  "Kevin Kruse", new Date(2018, 5, 4), 0 , 0),
+    new Quotes(2, "Life is what happens to you while you’re busy making other plans.", "John Lennon", new Date(2018, 6, 8), 0 , 0),
+    new Quotes( 3,"We become what we think about.", "Earl Nightingale", new Date(2018, 6, 9), 0 , 0), 
+    new Quotes(5, "Life is 10% what happens to me and 90% of how I react to it.", "Charles Swindoll", new Date(2018, 1, 12), 0 , 0),
+    new Quotes(6, "The most common way people give up their power is by thinking they don’t have any.", "Alice Walker", new Date(2018, 12, 5),0 ,0),
+    new Quotes(7, "The mind is everything. What you think you become.", "Buddha", new Date(2018, 4, 4),0 ,0)
  ]
 
  addNewQuote(quotes){
@@ -40,8 +40,16 @@ export class QuotesComponent implements OnInit{
   }
 }
 
-upvote(index){
-  
+
+upvote(i){
+  this.quotes[i].upVote()
+  console.log(this.quotes) 
+}
+downvote(i){
+  this.quotes[i].downVote()
 }
 
+
 }
+
+
